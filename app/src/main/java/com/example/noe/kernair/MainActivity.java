2337a2eba0522a2db884413345f7cb57adb84c95
Shabadoo2 <42898905+Shabadoo2@users.noreply.github.com>
2018-11-12 17:36:41 -0800

    adding in Navigation Hub Menu and testing Java file.

--- app/src/main/java/com/example/noe/kernair/MainActivity.java
+++ app/src/main/java/com/example/noe/kernair/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.content.Context;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -12,5 +14,16 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Alert NavigationHub
+        // super.onStart();
+        // setContentView(R.layout.activity_navigation_hub);
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
+
+
 }
