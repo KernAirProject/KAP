25df1a9c9e6e2c49cc46bb728e2d6c8e6c3b8d88
noeRamirez <noe.ramirez1997@email.bakersfieldcollege.edu>
2018-12-17 09:33:39 -0800

    Non-working version (2)

--- app/src/main/java/com/example/noe/kernair/FetchPA.java
+++ app/src/main/java/com/example/noe/kernair/FetchPA.java
@@ -1,6 +1,13 @@
 package com.example.noe.kernair;
 
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,13 +17,17 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class FetchPA extends AsyncTask<Void, Void, Void> {
+public class FetchPA extends AsyncTask<URL, Void, Void> {
 
 
     // Using this YouTube link: https://www.youtube.com/watch?v=Vcn4OuV4Ixg
-    String pAData;
+
+    String pAData;  //The raw data from the Purple Air JSON
+    JSONObject JO;
+    JSONArray JA;
+
     @Override
-    protected Void doInBackground(Void... voids) {
+    protected Void doInBackground(URL... URL) {
         try {
             URL purpleAirUrl = new URL("https://www.purpleair.com/json");
             try {
@@ -24,11 +35,22 @@ public class FetchPA extends AsyncTask<Void, Void, Void> {
                 InputStream inputStream = purpleAirUrlConnection.getInputStream();
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 String purpleAirStr = "";
-                while(purpleAirStr != null){
+                while((purpleAirStr = bufferedReader.readLine()) != null){
                     purpleAirStr = bufferedReader.readLine();
                     pAData = pAData + purpleAirStr;
                 }
+                
+                JA = new JSONArray(pAData);
+                JO = new JSONObject(pAData);
+                /**
+                for(int i = 0; i < JA.length(); i++){
+                    JO = (JSONObject) JA.get(i);
+                }
+                 */
+
             } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
         } catch (MalformedURLException e) {
@@ -38,8 +60,48 @@ public class FetchPA extends AsyncTask<Void, Void, Void> {
         return null;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
+        try {
+            MainActivity.AQITxtView.setText(JO.getInt("ID"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Sets the background color and
+     * the display value into the text view
+     * @param value: The AQI reading
+     * (Local variables) AQITxtView: The text view that is being edited
+     *                   AQITxtDisplay: The value being shown in the text view
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void AQIDisplay(int value){
+
+        String AQITxtDisplay = "AQI\n" + value;
+        MainActivity.AQITxtView.setText(AQITxtDisplay);
+
+        // if AQI is good
+        if(value >= 0 && value <= 50){
+            MainActivity.AQITxtView.setBackgroundColor(0xe07fa94c);  // Color value is hard coded for now
+            // if AQI is moderate
+        }else if(value >= 51 && value <= 100){
+            MainActivity.AQITxtView.setBackgroundColor(0xe0ffff00);
+            // if AQI is unhealthy for sensitive groups
+        }else if(value >= 101 && value <= 150){
+            MainActivity.AQITxtView.setBackgroundColor(0xe0FF9900);
+            // if AQI is unhealthy
+        }else if(value >= 151 && value <= 200){
+            MainActivity.AQITxtView.setBackgroundColor(0xe0FF3333);
+            // if AQI is very unhealthy
+        }else if(value >= 201 && value <= 300){
+            MainActivity.AQITxtView.setBackgroundColor(0xe0572D57);
+            // if AQI is hazardous
+        }else if(value >= 301){
+            MainActivity.AQITxtView.setBackgroundColor(0xe0800000);
+        }
     }
 }
--- app/src/main/java/com/example/noe/kernair/MainActivity.java
+++ app/src/main/java/com/example/noe/kernair/MainActivity.java
@@ -12,10 +12,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
+    public static TextView AQITxtView;
+    private FetchPA process = new FetchPA();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AQITxtView = findViewById(R.id.AQITxtView);
 
         // Attempts to launch the Kern Air Project twitter
         Button twitterBtn = findViewById(R.id.twitterBtn);
@@ -48,48 +53,7 @@ public class MainActivity extends AppCompatActivity{
                 }
             }
         });
-    }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    protected void onResume() {
-        super.onResume();
-        int aqi = 200;
-        AQIDisplay(aqi);
-    }
-
-    /**
-     * Sets the background color and
-     * the display value into the text view
-     * @param value: The AQI reading
-     * (Local variables) AQITxtView: The text view that is being edited
-     *                   AQITxtDisplay: The value being shown in the text view
-     */
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void AQIDisplay(int value){
-
-        TextView AQITxtView = findViewById(R.id.AQITxtView);
-        String AQITxtDisplay = "AQI\n" + value;
-        AQITxtView.setText(AQITxtDisplay);
-
-        // if AQI is good
-        if(value >= 0 && value <= 50){
-            AQITxtView.setBackgroundColor(0xe07fa94c);  // Color value is hard coded for now
-        // if AQI is moderate
-        }else if(value >= 51 && value <= 100){
-            AQITxtView.setBackgroundColor(0xe0ffff00);
-        // if AQI is unhealthy for sensitive groups
-        }else if(value >= 101 && value <= 150){
-            AQITxtView.setBackgroundColor(0xe0FF9900);
-        // if AQI is unhealthy
-        }else if(value >= 151 && value <= 200){
-            AQITxtView.setBackgroundColor(0xe0FF3333);
-        // if AQI is very unhealthy
-        }else if(value >= 201 && value <= 300){
-            AQITxtView.setBackgroundColor(0xe0572D57);
-        // if AQI is hazardous
-        }else if(value >= 301){
-            AQITxtView.setBackgroundColor(0xe0800000);
-        }
+        process.execute();
     }
 }
