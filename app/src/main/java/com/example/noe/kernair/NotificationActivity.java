df1393ffaf34ac3ecf7b1390ea6e319ef9be4fa0
Shabadoo2 <42898905+Shabadoo2@users.noreply.github.com>
2019-01-08 20:20:51 -0800

    Firebase Notification Commit with errors for save point. Do not test.

---
+++ app/src/main/java/com/example/noe/kernair/NotificationActivity.java
@@ -0,0 +1,4 @@
+package com.example.noe.kernair;
+
+public class NotificationActivity {
+}
--- app/src/main/java/com/example/noe/kernair/SplashActivity.java
+++ app/src/main/java/com/example/noe/kernair/SplashActivity.java
@@ -4,22 +4,51 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.*;
+import android.util.Log;
+import com.google.firebase.auth.*;
+import com.google.android.gms.tasks.*;
+
 
 public class SplashActivity extends AppCompatActivity {
     // Timeout in milliseconds
     private static int SPLASH_TIME_OUT = 1000;
+    private static final String TAG = "SplashActivity";
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run(){
-                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(mainIntent);
-                finish();
-            }
-        },SPLASH_TIME_OUT);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            // User is logged in
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent mainIntent = new Intent(getApplicationContext(), NotificationActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        } else {
+            setContentView(R.layout.activity_notification);
+        }
     }
 }
+
+
