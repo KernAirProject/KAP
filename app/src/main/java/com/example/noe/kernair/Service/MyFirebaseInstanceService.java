6e63a99428959e4b851cf1fa9acf61cfa03beb25
Shabadoo2 <42898905+Shabadoo2@users.noreply.github.com>
2019-01-10 19:57:52 -0800

    Firebase Notification Commit with errors for save point. Firebase Dependencies for cloud messaging won't configure correctly. Builds fine. Feel free to test.

---
+++ app/src/main/java/com/example/noe/kernair/Service/MyFirebaseInstanceService.java
@@ -0,0 +1,9 @@
+package com.example.noe.kernair.Service;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceService extends FirebaseInstanceIdService {
+    public void onTokenRefresh () {
+        super.onTokenRefresh();
+    }
+}
